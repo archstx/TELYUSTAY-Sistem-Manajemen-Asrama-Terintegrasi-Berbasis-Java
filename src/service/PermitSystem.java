090be943139c80713a00012ebd8187cb51de7d03
Archanist <116186214+archstx@users.noreply.github.com>
2026-06-22 23:14:24 +0700

    Delete src directory

--- src/service/PermitSystem.java
+++
@@ -1,9 +0,0 @@
-package asrama.interfacepkg;
-
-import asrama.model.Mahasiswa;
-
-public interface PermitSystem {
-    boolean applyPermit(Mahasiswa mahasiswa);
-
-    boolean validateStatus(Mahasiswa mahasiswa);
-}
